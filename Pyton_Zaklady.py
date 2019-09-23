17b72a2ef3c1df0d4ef1453a4ed3f32ba3d498e6
Robert Chalcak <robochalcak@gmail.com>
2019-09-23 14:08:43 +0200

    Initial commit

---
+++ Pyton_Zaklady.py
@@ -0,0 +1,4 @@
+zvieratka=["macka","pes","kocur","sliepka","prasa"];  #array with content
+
+for zviera in zvieratka:  #makes an iteration via for loop trought array of "zvieratka"
+    print (zviera)      #print content of array "zvieratka"
